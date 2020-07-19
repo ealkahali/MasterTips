5ea6c050a339cfb2f73da1b25d42687c7f96d5a6
Eman Alkahali <emanalkahali@Emans-MacBook-Pro.local>
2020-07-19 18:02:18 -0400

    first commit

--- forms/notify.php
+++ forms/notify.php
@@ -11,7 +11,8 @@
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
-  } 
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
