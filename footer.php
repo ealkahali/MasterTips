fa0c00c8890d040f3a06979e2fcd1b6ccbd07ade
Eman Alkahali <emanalkahali@Emans-MacBook-Pro.local>
2020-07-19 17:27:14 -0400

    first commit

--- footer.php
+++
@@ -1,6 +0,0 @@
-<?php $zunday_theme_options = get_option('zunday'); ?>         
-</div>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php $zunday_theme_options = get_option('zunday'); ?>
-<?php get_header(); ?>
-<?php include ( get_parent_theme_file_path() . '/includes/gn-loop.php' ); ?>
-<?php get_footer(); ?>
